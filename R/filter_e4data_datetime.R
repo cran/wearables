eabe878b85d6ee9e8e79a2ef7087d6b0b6c13a40
Peter de Looff <peterdelooff@gmail.com>
2021-06-30 14:20:02 +0000

    version 0.6.2

---
+++ R/filter_e4data_datetime.R
@@ -0,0 +1,33 @@
+#' Filter all four datasets for a Datetime start + end
+#' @param data Object read with \code{\link{read_e4}}
+#' @param start Start Datetime (posixct)
+#' @param end End Datetime (posixct)
+#' @export
+#' @importFrom dplyr .data
+filter_e4data_datetime <- function(data, start, end){
+  
+  data$IBI$datetime <- lubridate::force_tz(data$IBI$DateTime, "UTC")
+  data$EDA$datetime <- lubridate::force_tz(data$EDA$DateTime, "UTC")
+  data$ACC$datetime <- lubridate::force_tz(data$ACC$DateTime, "UTC")
+  data$TEMP$datetime <- lubridate::force_tz(data$TEMP$DateTime, "UTC")
+  data$HR$datetime <- lubridate::force_tz(data$HR$DateTime, "UTC")
+  
+  data$IBI <- dplyr::filter(data$IBI, 
+                            .data$datetime >= start,
+                            .data$datetime <= end)
+  data$EDA <- dplyr::filter(data$EDA, 
+                            .data$datetime >= start,
+                            .data$datetime <= end)
+  data$ACC <- dplyr::filter(data$ACC, 
+                            .data$datetime >= start,
+                            .data$datetime <= end)
+  data$TEMP <- dplyr::filter(data$TEMP, 
+                             .data$datetime >= start,
+                             .data$datetime <= end)
+  data$HR <- dplyr::filter(data$HR, 
+                           .data$datetime >= start,
+                           .data$datetime <= end)
+  
+  return(data)
+}
+
---
+++ R/utils.R
@@ -0,0 +1,57 @@
+
+#' as_time
+#' @description Converts Unix time to as.POSIXct 
+#' @param x takes a unixtime and converts to as.POSIXct
+#' @param tz timezone is set to UTC
+#' @export
+# Convert time in seconds to a POSIXct.
+as_time <- function(x, tz = "UTC"){
+  as.POSIXct(x, origin = "1970-1-1", tz = tz)
+}
+
+
+#' prepend_time_column
+#' @description Column binds a time_column to the dataframe
+#' @param data dataframe
+#' @param timestart the start of the recording
+#' @param hertz hertz in which the E4 data was recorded
+#' @param tz The timezone, defaults to user timezone
+#' @export 
+#' @importFrom lubridate with_tz
+prepend_time_column <- function(data, timestart, hertz, tz = Sys.timezone()){
+
+  datetime <-  as_time(timestart) + (1/hertz) * (1:nrow(data) - 1)
+
+  datetime <- with_tz(datetime, tz)
+
+  out <- cbind(data.frame(DateTime = datetime), data)
+
+  return(out)
+}
+
+#' pad_e4
+#' @description function to combine several e4 files, and sets the length of the x-axis
+#' @param x index of dataframe
+#' @export
+#' @importFrom dplyr left_join
+#' @importFrom stats median
+#' @importFrom utils unzip
+#' @importFrom utils read.csv
+pad_e4 <- function(x){
+
+  interval <- as.numeric(median(diff(x$DateTime)))
+
+  out <- data.frame(DateTime = seq(from = min(x$DateTime),
+                                   to = max(x$DateTime),
+                                   by = interval)
+  )
+
+left_join(out, x, by = "DateTime")
+}
+
+
+
+
+
+
+
